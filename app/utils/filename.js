e990e15eced73913446d1f6119deb814abe8760c
José San Gil <jsangilve@gmail.com>
2016-05-29 11:29:28 +0200

    fix(books): adds slufify helper and fixes bugs on books template

--- app/utils/filename.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-const { dasherize } = Ember.String;
-
-export default function filename(name) {
-  let fname = name.replace(/[^a-zA-Z0-9]+/g, ' ');
-  return dasherize(fname);
-}
