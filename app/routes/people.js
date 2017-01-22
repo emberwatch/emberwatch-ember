b5d8fed0348751eed0d14ec5630a2dd27252eac0
Jure Cuhalev <jure@cuhalev.com>
2017-01-22 15:53:30 +0100

    feat(titles) Implements document.title with ember-cli-document-title

--- app/routes/people.js
+++ app/routes/people.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 const { Route } = Ember;
 
 export default Route.extend({
+  titleToken: 'People of Ember.js community',
   model() {
     return this.store.findAll('person');
   }
