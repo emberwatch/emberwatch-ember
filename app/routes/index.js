6c10d6a12d5a0fb6d54a90cb03c2d5bed53d1f26
Jure Cuhalev <jure@cuhalev.com>
2017-02-10 22:27:35 +0100

    feat(homepage) Basic homepage that displays latest talks and tutorials

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,21 @@
 import Ember from 'ember';
 
-const { Route } = Ember;
+const {
+  Route,
+  RSVP
+} = Ember;
 
 export default Route.extend({
-  beforeModel() {
-    this.transitionTo('talks');
+  titleToken: 'Learn about Ember.js',
+  model() {
+    return RSVP.hash({
+      talks: this.store.findAll('talk').then((result) => result.toArray().reverse().slice(0, 3)),
+      tutorials: this.store.findAll('tutorial').then((result) => result.toArray().reverse().slice(0, 3))
+    });
+  },
+
+  setupController(controller, models) {
+    controller.setProperties(models);
   }
+
 });
---
+++ tests/acceptance/index-test.js
@@ -0,0 +1,15 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'emberwatch-ember/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | index');
+
+test('visiting /', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+    assert.equal(find('.hello h1').text().trim(), 'Welcome to EmberWatch');
+
+  });
+});
+
